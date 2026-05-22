73e196ba93c8867b32b56171b8de5617d066f0ae
oleksandra <sasha.shergina@gmail.com>
2026-05-22 12:13:56 +0300

    removed default api and added hello world function

--- handler.js
+++ handler.js
@@ -2,22 +2,7 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
+exports.hello = async () => {
+  return "Hello World!";
+};
 
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
-
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
-
-exports.handler = serverless(app);
